ee8aaebe053ea6c43d9e398ac3e3d819d1210d45
Lee SH <seunghoon.no.1@gmail.com>
2023-09-27 15:05:30 +0900

    0927

---
+++ 4w/Test1.java
@@ -0,0 +1,12 @@
+public class Test1
+{
+    public static void main(String[] args)
+    {
+        int[] topping = { 10, 20, 30 };
+
+        for (int value: topping)
+        {
+            System.out.print(value + " ");
+        }
+    }
+}
